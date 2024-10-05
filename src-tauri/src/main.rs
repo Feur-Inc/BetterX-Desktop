ad68cbc15011b717b0f63e78e75738582a0e6278
MopigamesYT <mopigames2.0@gmail.com>
2024-10-05 23:50:52 +0200

    Refactor main.rs and tauri.conf.json to inject BetterX bundle
    - Refactor main.rs to read the BetterX bundle from file and inject it into the webview window.
    - Update tauri.conf.json to include the bundle.js file in the resources section.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,19 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+use std::fs;
 use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
       let window = app.get_webview_window("main").unwrap();
-      
+     
       // Set the window title
       window.set_title("BetterX Desktop").unwrap();
 
-      window.eval("
-        console.log('ceci est du code injecté, hello world !');
-      ").unwrap();
+      // Read the BetterX bundle
+      let betterx_js = fs::read_to_string("./bundle.js").expect("Unable to read BetterX bundle");
+
+      // Inject BetterX
+      window.eval(&format!("
+        {}
+        console.log('BetterX injected successfully');
+      ", betterx_js)).unwrap();
 
       Ok(())
     })
