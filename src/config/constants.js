3971aea06ed297c49aadd5c0d10fbe8a59fd31b0
MopigamesYT <mopigames2.0@gmail.com>
2024-10-14 17:55:37 +0200

    Revert "Separated main.js in multiple files"
    This reverts commit fe6dbcf5a867b85a2d5f4ed12219bce1c4642e45.

--- src/config/constants.js
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import { app } from 'electron';
-
-export const BUNDLE_URL = 'https://feur-inc.github.io/BetterX/desktop/bundle.js';
-export const TEST_UPDATE_MODE = false;
-export const BETTERX_PATH = path.join(app.getPath('appData'), 'BetterX');
-export const SETTINGS_PATH = path.join(BETTERX_PATH, 'desktop.settings.json');
