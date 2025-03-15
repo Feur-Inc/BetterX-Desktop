893d421c8ef02e3170676d4fe53e4b1c83ed645a
Margot <mopigames2.0@gmail.com>
2025-03-15 18:35:17 +0100

    update nix & push to 2.0.1-beta

--- nix/betterx-desktop.nix
+++ nix/betterx-desktop.nix
@@ -2,18 +2,46 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "betterx-desktop";
-  version = "1.0.1-alpha";
-  src = ./.;
+  version = "2.0.1-beta";
+  src = ../.;
 
-  nativeBuildInputs = [ pkgs.nodejs ];
+  nativeBuildInputs = [ 
+    pkgs.nodejs 
+    pkgs.nodePackages.npm
+    pkgs.electron
+  ];
+
+  buildInputs = [
+    pkgs.makeWrapper
+  ];
 
   buildPhase = ''
-    npm install
+    export HOME=$PWD
+    npm ci
+    npm run build:linux
   '';
 
   installPhase = ''
-    mkdir -p $out
-    cp -r * $out/
+    mkdir -p $out/bin $out/share/applications
+    
+    # Copy the built application
+    cp -r dist/linux-unpacked/* $out/
+    
+    # Create a wrapper script
+    makeWrapper $out/betterx-desktop $out/bin/betterx-desktop \
+      --set ELECTRON_IS_DEV 0
+    
+    # Create desktop entry
+    cat > $out/share/applications/betterx-desktop.desktop << EOF
+    [Desktop Entry]
+    Type=Application
+    Name=BetterX Desktop
+    Comment=Desktop application for BetterX, enhancing your browsing experience
+    Exec=$out/bin/betterx-desktop
+    Icon=$out/resources/icon.png
+    Categories=Network;WebBrowser;
+    Terminal=false
+    EOF
   '';
 
   meta = with pkgs.lib; {
