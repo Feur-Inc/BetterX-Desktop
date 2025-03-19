99dab7853218ede858a808fd56d621d39fc424dd
Mopigamesyt <mopigames2.0@gmail.com>
2025-03-19 14:04:20 +0100

    Update BASE_URL for production deployment

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import path from 'path';
 import { app } from 'electron';
 
 // For local testing
-export const BASE_URL = 'https://feur-inc.github.io/BetterX/desktop/v2/bundle.js';
+export const BASE_URL = 'https://feur-inc.github.io/BetterX/desktop/v2';
 export const BUNDLE_URL = `${BASE_URL}/bundle.js`;
 export const TEST_UPDATE_MODE = false;
 export const BETTERX_PATH = path.join(app.getPath('appData'), 'BetterX');
